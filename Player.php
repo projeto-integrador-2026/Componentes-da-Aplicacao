6ace985ac3a37905f2603baecf46d59825e095a6
Jefferson Chaves <jefferson.chaves@ifpr.edu.br>
2026-03-19 12:13:46 -0300

    Add Player class with properties

---
+++ Player.php
@@ -0,0 +1,19 @@
+namespace app\models;
+
+use DateTimeImmutable;
+
+class Player {
+
+    private int $id;
+    private string $name;
+    private string $birthDate;
+    private ?string $nationality;
+    private ?float $height;
+    private ?float $weight;
+    private ?string $dominantFoot;
+    private ?string $position;
+    private ?string $team;
+    private ?string $image;
+    private DateTimeImmutable $created_at;
+
+}
